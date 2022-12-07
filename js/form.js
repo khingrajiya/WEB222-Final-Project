34401404f8c5e847e2c1da631f467ad2a0750bef
Krupali Dineshbhai Hingrajiya <113001599+khingrajiya@users.noreply.github.com>
2022-12-06 19:54:06 -0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,22 @@
+/*
+ * WEB222 – Final Assessment
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ *      Name:       Krupali Dineshbhai Hingrajiya
+ *      Student ID: 
+ *      Date:       07 December 2022
+ */
+
+function other() {
+    var wage = document.getElementById("other");
+  
+    if (document.getElementById("other").checked) {
+      wage.style.display = "inline";
+    } else {
+      wage.style.display = "none";
+    }
+  }
